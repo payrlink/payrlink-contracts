49513f7ac531ed94dd62f77db022790a2a5314e0
Magic Dev <magicrjh0619@gmail.com>
2021-05-03 19:21:10 +1000

    Use for in lp add

--- migrations/2_deploy_farm.js
+++ migrations/2_deploy_farm.js
@@ -10,35 +10,38 @@ module.exports = async function(deployer) {
   try {
     let dataParse = contracts;
 
-    if (!configs.Farm) {
-      const currentBlock = await web3.eth.getBlockNumber();
-      const startBlock = configs.farm_param.startBlock
-          || web3.utils.toBN(currentBlock).add(web3.utils.toBN(configs.farm_param.delay));
+    if (configs.lpExist) {
+      if (!configs.Farm) {
+        const currentBlock = await web3.eth.getBlockNumber();
+        const startBlock = configs.farm_param.startBlock
+            || web3.utils.toBN(currentBlock).add(web3.utils.toBN(configs.farm_param.delay));
 
-      await deployer.deploy(Farm, dataParse['PAYR'], web3.utils.toBN(configs.farm_param.rewardPerBlock), startBlock, {
-        gas: 5000000
-      });
-      const farmInstance = await Farm.deployed();
-      dataParse['Farm'] = Farm.address;
+        await deployer.deploy(Farm, dataParse['PAYR'], web3.utils.toBN(configs.farm_param.rewardPerBlock), startBlock, {
+          gas: 5000000
+        });
+        const farmInstance = await Farm.deployed();
+        dataParse['Farm'] = Farm.address;
 
-      if (configs.farm_param.fund) {
-        const payrInstance = await PAYR.at(dataParse['PAYR']);
-        await payrInstance.approve(Farm.address, web3.utils.toBN(configs.farm_param.fund));
-        await farmInstance.fund(web3.utils.toBN(configs.farm_param.fund));
-      }
-
-      configs.farm_param.lp.forEach(async (token) => {
-        if (token.address) {
-          await farmInstance.add(
-            token.allocPoint,
-            token.address,
-            false
-          );
+        if (configs.farm_param.fund) {
+          const payrInstance = await PAYR.at(dataParse['PAYR']);
+          await payrInstance.approve(Farm.address, web3.utils.toBN(configs.farm_param.fund));
+          await farmInstance.fund(web3.utils.toBN(configs.farm_param.fund));
         }
-      })
-    }
-    else {
-      dataParse['Farm'] = configs.Farm;
+
+        for (let i = 0; i < configs.farm_param.lp.length; i ++) {
+          const token = configs.farm_param.lp[i];
+          if (token.address) {
+            await farmInstance.add(
+              token.allocPoint,
+              token.address,
+              false
+            );
+          }
+        }
+      }
+      else {
+        dataParse['Farm'] = configs.Farm;
+      }      
     }
 
     const updatedData = JSON.stringify(dataParse);
