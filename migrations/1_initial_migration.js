4bb27c83a09c37124aeff2d4e9dd58228cefd487
Cedric <cedric.pk22@gmail.com>
2021-06-01 15:45:09 -0400

    Update Crowdsale for Publish

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -10,10 +10,11 @@ module.exports = async function(deployer) {
   try {
     let dataParse = {};
 
-    const startOfPresale = Math.floor(Date.UTC(2021, 3, 1, 0, 0, 0) / 1000);
-    const endOfPresale = Math.floor(Date.UTC(2021, 3, 2, 0, 0, 0) / 1000);
-    const startOfICO = Math.floor(Date.UTC(2021, 3, 3, 0, 0, 0) / 1000);
-    const endOfICO = Math.floor(Date.UTC(2021, 3, 7, 0, 0, 0) / 1000);
+    // const startOfPresale = Math.floor(Date.UTC(2021, 3, 1, 0, 0, 0) / 1000);
+    // const endOfPresale = Math.floor(Date.UTC(2021, 3, 2, 0, 0, 0) / 1000);
+    const startOfICO = Math.floor(Date.UTC(2021, 5, 4, 0, 0, 0) / 1000);
+    const endOfICO = Math.floor(Date.UTC(2021, 5, 22, 0, 0, 0) / 1000);
+    const publishDate = Math.floor(Date.UTC(2021, 5, 23, 0, 0, 0) / 1000);
 
     if (!configs.PAYR) {
       await deployer.deploy(PAYR, {
@@ -27,18 +28,18 @@ module.exports = async function(deployer) {
       dataParse['PAYR'] = configs.PAYR;
     }
   
-    if (!configs.Presale) {
-      await deployer.deploy(Presale, dataParse['PAYR'], startOfPresale, endOfPresale, endOfICO, {
-        gas: 5000000
-      });
-      dataParse['Presale'] = Presale.address;
-    }
-    else {
-      dataParse['Presale'] = configs.Presale;
-    }
+    // if (!configs.Presale) {
+    //   await deployer.deploy(Presale, dataParse['PAYR'], startOfPresale, endOfPresale, endOfICO, {
+    //     gas: 5000000
+    //   });
+    //   dataParse['Presale'] = Presale.address;
+    // }
+    // else {
+    //   dataParse['Presale'] = configs.Presale;
+    // }
   
     if (!configs.Crowdsale) {
-      await deployer.deploy(Crowdsale, dataParse['PAYR'], startOfICO, endOfICO, {
+      await deployer.deploy(Crowdsale, dataParse['PAYR'], startOfICO, endOfICO, publishDate, {
         gas: 5000000
       });
       dataParse['Crowdsale'] = Crowdsale.address;
